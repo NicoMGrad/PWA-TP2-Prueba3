be33bd3e5c574e9622943cd4a3562ab613f8d9c9
Nicolás Alejandro mercado Grad <nicolas.mercado@davinci.edu.ar>
2021-06-26 05:03:04 -0300

    asdasd

--- main.js
+++ main.js
@@ -71,12 +71,11 @@ const   base_URL = 'https://api.jikan.moe/v3/search/anime',
                     <div class="supporting-text">${anime.synopsis}</div>
                     <div class="actions">
                         <div class="action-buttons">
-                            <button class="button buttonCard btnMiRipple" type="button" onclick="window.location.href='${anime.url}'">+</button>
+                            <button class="button buttonCard miRipple" type="button" onclick="window.location.href='${anime.url}'">+</button>
                         </div>
                     </div>
                 </div>
                 `
-
             }).join("");
     }
     
