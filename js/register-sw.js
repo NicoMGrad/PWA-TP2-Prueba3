ae82da89fca08a12a6d73b697370f8c96ca8834c
Nicolás Alejandro mercado Grad <nicolas.mercado@davinci.edu.ar>
2021-07-01 11:47:23 -0300

    asd

--- js/register-sw.js
+++ js/register-sw.js
@@ -93,7 +93,7 @@ else {
 
 window.addEventListener('offline', event => {
   main.innerHTML = cuadroTetera + resultadosHistoria;
-  location.reload();
+  //location.reload();
   if (window.matchMedia('max-width:800px;')) {
     divBusqueda.style.marginTop = '7rem';
   } else {
@@ -120,7 +120,7 @@ window.addEventListener('online', event => {
 
 if (!navigator.onLine) {
   main.innerHTML = cuadroTetera + resultadosHistoria;
-  location.reload();
+  //location.reload();
   if (window.matchMedia('max-width:800px;')) {
     divBusqueda.style.marginTop = '7rem';
   } else {
